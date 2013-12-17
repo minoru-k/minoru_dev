3e258b2d364304696890e3592cc5be6c20b3ce10
minoru-k <minoru.k0804@gmail.com>
2013-12-17 20:42:07 +0900

    練習用テストコードの追加
    storage.jsのテスト方法を模索している。

---
+++ storageTest.js
@@ -0,0 +1,18 @@
+﻿function $Test() {
+    document.getElementById("test1").value = "テスト";
+    
+    var expect = document.getElementById("test1").value;
+    var target = $("test1").value;
+
+    if (expect === target) {
+        document.getElementById("result").innerHTML = "OK";
+    } else {
+        document.getElementById("result").innerHTML = "NG";
+    }
+}
+
+window.addEventListener("load", function() {
+    document.getElementById("execButton").addEventListener("click", function() {
+        $Test();
+    });
+});
